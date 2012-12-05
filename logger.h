5bf9966efc1faa972d9a18c79d1ada437ccc411b
f-kubotar <dev@hadashikick.jp>
2012-12-06 01:06:29 +0900

    loggerヘッダだけにした

--- logger.h
+++ logger.h
@@ -3,6 +3,10 @@
 
 #include <string>
 
+#include <stdio.h>
+#include <time.h>
+#include <stdarg.h>
+
 #define IIINFO(s, ...)   kawaii::log("INFO", s, ##__VA_ARGS__)
 #define IIWARN(s, ...)   kawaii::log("WARN", s, ##__VA_ARGS__)
 #define IIERROR(s, ...)  kawaii::log("ERROR", s, ##__VA_ARGS__)
@@ -10,7 +14,27 @@
 namespace kawaii {
 using namespace std;
 
-void log(const char *loglevel, const string message, ...);
+static void log(const char *loglevel, const string message, ...) {
+    time_t now = time(NULL);
+    struct tm *ts = localtime(&now);
+    printf("kawaii - %04d-%02d-%02d %02d:%02d:%02d [%s] ",
+           ts->tm_year + 1900,
+           ts->tm_mon + 1,
+           ts->tm_mday,
+           ts->tm_hour,
+           ts->tm_min,
+           ts->tm_sec,
+           loglevel
+           );
+
+    va_list args;
+    va_start(args, message);
+    vprintf(message.c_str(), args);
+    va_end(args);
+
+    printf("\n");
+}
+
 }
 
 #endif  /* defined(__kawaii__logger__) */
