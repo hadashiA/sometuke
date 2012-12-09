92dde9390af0f64d5224880e065caa0127f0e9ad
f-kubotar <dev@hadashikick.jp>
2012-12-09 16:56:48 +0900

    Texture2D名前空間に定数をうつす

--- texture_2d.cc
+++ texture_2d.cc
@@ -3,14 +3,21 @@
 namespace kawaii {
 
 Texture2D::Texture2D() : id_(0), pixelSize_(0, 0), contentSize_(0, 0),
-                         format_(kTexture2DPixelFormat_Default) {
+                         format_(kPixelFormat_Default) {
     glGenTextures(1, &id_);
     glBindTexture(GL_TEXTURE_2D, id_);
 }
 
-void Texture2D::LoadData(const void* data, Texture2DPixelFormat pixelFormat,
-                         ivec2 pixelSize_, vec2 contentSize_) {
-    
+void Texture2D::LoadData(const void* data, PixelFormat pixelFormat,
+                         ivec2 pixelSize, vec2 contentSize) {
+    if (pixelFormat == kPixelFormat_RGBA8888) {
+        
+    }
 }
 
 }
+
+
+
+
+
--- texture_2d.h
+++ texture_2d.h
@@ -8,44 +8,44 @@
 
 namespace kawaii {
 
-typedef enum {
-    //! 32-bit texture: RGBA8888
-    kTexture2DPixelFormat_RGBA8888,
-    //! 32-bit texture without Alpha channel. Don't use it.
-    kTexture2DPixelFormat_RGB888,
-    //! 16-bit texture without Alpha channel
-    kTexture2DPixelFormat_RGB565,
-    //! 8-bit textures used as masks
-    kTexture2DPixelFormat_A8,
-    //! 8-bit intensity texture
-    kTexture2DPixelFormat_I8,
-    //! 16-bit textures used as masks
-    kTexture2DPixelFormat_AI88,
-    //! 16-bit textures: RGBA4444
-    kTexture2DPixelFormat_RGBA4444,
-    //! 16-bit textures: RGB5A1
-    kTexture2DPixelFormat_RGB5A1,
-    //! 4-bit PVRTC-compressed texture: PVRTC4
-    kTexture2DPixelFormat_PVRTC4,
-    //! 2-bit PVRTC-compressed texture: PVRTC2
-    kTexture2DPixelFormat_PVRTC2,
-    
-    //! Default texture format: RGBA8888
-    kTexture2DPixelFormat_Default = kTexture2DPixelFormat_RGBA8888,
-
-} Texture2DPixelFormat;
-
 class Texture2D {
 public:
+    typedef enum {
+        //! 32-bit texture: RGBA8888
+        kPixelFormat_RGBA8888,
+        //! 32-bit texture without Alpha channel. Don't use it.
+        kPixelFormat_RGB888,
+        //! 16-bit texture without Alpha channel
+        kPixelFormat_RGB565,
+        //! 8-bit textures used as masks
+        kPixelFormat_A8,
+        //! 8-bit intensity texture
+        kPixelFormat_I8,
+        //! 16-bit textures used as masks
+        kPixelFormat_AI88,
+        //! 16-bit textures: RGBA4444
+        kPixelFormat_RGBA4444,
+        //! 16-bit textures: RGB5A1
+        kPixelFormat_RGB5A1,
+        //! 4-bit PVRTC-compressed texture: PVRTC4
+        kPixelFormat_PVRTC4,
+        //! 2-bit PVRTC-compressed texture: PVRTC2
+        kPixelFormat_PVRTC2,
+    
+        //! Default texture format: RGBA8888
+        kPixelFormat_Default = kPixelFormat_RGBA8888,
+
+    } PixelFormat;
+
+
     Texture2D();
 
-    void LoadData(const void* data, Texture2DPixelFormat pixelFormat,
-                  ivec2 pixelSize_, vec2 contentSize_);
+    void LoadData(const void* data, PixelFormat pixelFormat, ivec2 pixelSize_, vec2 contentSize_);
 private:
     GLuint id_;
     ivec2 pixelSize_;
     vec2 contentSize_;
-    Texture2DPixelFormat format_;
+    PixelFormat format_;
     GLfloat max_s_;
     GLfloat max_t_;
     bool has_premultipled_alpha_;
