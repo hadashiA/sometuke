978b25f562734b420e1bc002d5acf43f1e24fdb3
f-kubotar <dev@hadashikick.jp>
2012-12-09 16:31:35 +0900

    Texture2D constructor

--- texture_2d.h
+++ texture_2d.h
@@ -37,8 +37,7 @@ typedef enum {
 
 class Texture2D {
 public:
-    Texture2D() : id_(0), pixelSize_(0, 0), contentSize_(0, 0),
-                  format_(kTexture2DPixelFormat_Default) {}
+    Texture2D();
 
     void LoadData(const void* data, Texture2DPixelFormat pixelFormat,
                   ivec2 pixelSize_, vec2 contentSize_);
