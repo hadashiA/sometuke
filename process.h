06e0a700770c924a090b06880575f42e92dff9cb
f-kubotar <dev@hadashikick.jp>
2013-01-02 14:22:17 +0900

    added noncopyable Process class

---
+++ process.h
@@ -0,0 +1,24 @@
+#ifndef __kawaii__process__
+#define __kawaii__process__
+
+#include <memory>
+
+namespace kawaii {
+using namespace std;
+
+class Process {
+public:
+
+protected:
+    
+
+private:
+    Process(const Process&);
+    Process& operator=(const Process&);
+
+    
+};
+
+}
+
+#endif /* defined(__kawaii__process__) */
