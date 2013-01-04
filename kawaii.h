ee7ceec83fd28fb51658c4f5375f181270ba6aaa
f-kubotar <dev@hadashikick.jp>
2013-01-05 02:49:06 +0900

    kawaii.hヘッダ

--- kawaii.h
+++ kawaii.h
@@ -2,7 +2,19 @@
 #define __kawaii__
 
 #include "application.h"
+#include "director.h"
+
+// resource
+#include "texture_2d.h"
 #include "shader_cache.h"
+
+// node
+#include "scene.h"
+
+// structs
+#include "vector.h"
+#include "matrix.h"
+#include "vertices.h"
 
 namespace kawaii {
 using namespace std;
