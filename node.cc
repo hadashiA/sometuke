58a13022a99f8ec7b17152ba97e107f807d71e2e
f-kubotar <dev@hadashikick.jp>
2013-01-20 03:05:48 +0900

    implmented Node::NodeToParentTransform

--- node.cc
+++ node.cc
@@ -9,33 +9,109 @@ using namespace std;
 
 void Node::set_position(const vec3& position) {
     position_ = position;
-    is_transform_dirty_ = true;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
 }
 
 void Node::add_position(const vec3& diff) {
     position_ += diff;
-    is_transform_dirty_ = true;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
 }
 
 void Node::set_scale_x(const float scale_x) {
     scale_.x = scale_x;
-    is_transform_dirty_ = true;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
 }
 
 void Node::set_scale_y(const float scale_y) {
     scale_.y = scale_y;
-    is_transform_dirty_ = true;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
 }
 
 void Node::set_scale(const float scale) {
     scale_.x = scale;
     scale_.y = scale;
-    is_transform_dirty_ = true;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
+}
+
+void Node::set_skew_x(const float skew_x) {
+    skew_.x = skew_x;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
+}
+
+void Node::set_skew_y(const float skew_y) {
+    skew_.y = skew_y;
+    is_transform_dirty_ = is_inverse_dirty_ = true;
 }
 
 void Node::set_z_order(int z_order) {
     z_order = z_order;
     // parent_->ReorderChild(this, z_order)
+}
+
+void Node::set_anchor_point(const vec2& anchor_point) {
+}
+
+const mat4& Node::NodeToParentTransform() {
+    if (is_transform_dirty_) {
+        float x = position_.x;
+        float y = position_.y;
+        float z = position_.z;
+
+        float c = 1;
+        float s = 0;
+        if (rotation_) {
+            float radians = -DegreesToRadians(rotation_);
+            c = cosf(radians);
+            s = sinf(radians);
+        }
+
+        // optimization:
+        // inline anchor point calculation if skew is not needed
+        bool needs_skew_matrix = (skew_.x || skew_.y);
+        if (!needs_skew_matrix && !IsAnchorPointZero()) {
+            x += c * -anchor_point_.x * scale_.x + -s * -anchor_point_.y * scale_.y;
+            y += s * -anchor_point_.x * scale_.x +  c * -anchor_point_.y * scale_.y;
+        }
+
+        // Build Transform Matrix
+        float a = c * scale_.x;
+        float b = s * scale_.x;
+        float c = -s * scale_.y;
+        float d = c * scale_.y;
+        float tx = x;
+        float ty = y;
+        transform_ = mat4(a, b, 0, 0,
+                          c, d, 0, 0,
+                          0, 0, 1, 0,
+                          tx, ty, 0, 1);
+
+        // XXX: Try to inline skew
+        // If skew is needed, apply skew and then anchor point
+        if (needs_skew_matrix) { 
+            mat4 skew_matrix(1.0f, tanf(DegreesToRadians(skew_.y)), 0, 0,
+                             tanf(DegreesToRadians(skew_.x)), 1.0f, 0, 0,
+                             0, 0, 1, 0,
+                             0, 0, 0, 1);
+            transform_ = skew_matrix * transform_;
+
+            // adjust anchor point
+            if (!IsAnchorPointZero()) {
+                transform_ = transform_.Translate(-anchor_point_.x, -anchor_point_.y, 0);
+            }
+        }
+
+        is_transform_dirty_ = false;
+    }
+
+    return transform_;
+}
+
+const mat4& Node::ParentToNodeTransform() {
+    if (is_inverse_dirty_) {
+        // is_inverse_dirty_ = transform_.Inverse();
+        is_inverse_dirty_ = false;
+    }
+    return inverse_;
 }
 
 void Node::AddChild(shared_ptr<Node> child) {
--- node.h
+++ node.h
@@ -17,22 +17,32 @@ public:
         : position_(0, 0, 0),
           scale_(1, 1),
           rotation_(0, 0),
+          skew_(0, 0),
           anchor_point_(0, 0),
           content_size_(0, 0),
           z_order_(0),
           is_visible_(true),
           is_running_(false),
-          is_transform_dirty_(true) {
+          is_transform_dirty_(true),
+          is_inverse_dirty_(true) {
     }
     
     virtual ~Node() {}
 
-    const vec3& position() const {
-        return position_;
+    const vec3& local_position() const {
+        return local_position_;
     }
 
     const vec2& scale() const {
         return scale_;
+    }
+
+    const float rotation() const {
+        return rotation_;
+    }
+
+    const vec2& skew() const {
+        return skew_;
     }
 
     const int z_order() const {
@@ -47,12 +57,20 @@ public:
         return is_running_;
     }
 
-    void set_position(const vec3& position);
+    const vec2& anchor_point() const {
+        return anchor_point_;
+    }
+
+    void set_local_position(const vec3& local_position);
     void add_position(const vec3& diff);
     void set_scale_x(const float scale_x);
     void set_scale_y(const float scale_y);
-    void set_scale(const float scale);
+    void set_scale(const vec2& scale);
+    void set_skew_x(const float skew_x);
+    void set_skew_y(const float skew_y);
+    void set_rotation(const float rotation);
     void set_z_order(int z_order);
+    void set_anchor_point(const vec2& point);
 
     void show() {
         is_visible_ = true;
@@ -62,32 +80,46 @@ public:
         is_visible_ = false;
     }
 
-    virtual void AddChild(shared_ptr<Node> child);
-
-    Node& operator<<(shared_ptr<Node> child) {
-        AddChild(child);
-        return *this;
+    bool AnchorPointIsZero() const {
+        return (anchor_point_.x == 0 && anchor_point_.y == 0);
     }
 
-    void Visit();
-    void ScheduleUpdate();
+    const vec3& WorldPosition() const;
+    void SetWorldPosition(const vec3& world_position);
+
+    virtual void AddChild(shared_ptr<Node> child);
+
+    // Node& operator<<(shared_ptr<Node> child) { 
+    //     AddChild(child);
+    //     return *this;
+    // }
 
     virtual void Update(const ii_time delta_time) {}   // Is not called by default
     virtual void OnEnter() {}
     virtual void Render()  {}
     virtual void OnExit()  {}
 
+    void Visit();
+    void ScheduleUpdate();
+
+    const mat4& NodeToParentTransform();
+    const mat4& ParentToNodeTransform();
+
 private:    
-    vec3 position_;
+    vec3 local_position_;
     vec2 scale_;
-    vec2 rotation_;
+    float rotation_;
+    vec2 skew_;
     vec2 anchor_point_;
     vec2 content_size_;
     int z_order_;
     bool is_visible_;
     bool is_running_;
 
+    mat4 transform_;
+    mat4 inverse_;
     bool is_transform_dirty_;
+    bool is_inverse_dirty_;
 
     vector<shared_ptr<Node> > children_;
 };
