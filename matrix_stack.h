8d550239098da234e22fc54277f54a218c4db0ac
f-kubotar <dev@hadashikick.jp>
2013-01-23 22:40:52 +0900

    Revert "MatrixStackもinline化"
    This reverts commit f200a86c3d0e08301b3941b17a3efa0b7891b7f2.

--- matrix_stack.h
+++ matrix_stack.h
@@ -10,43 +10,14 @@ using namespace std;
 
 class MatrixStack {
 public:
-    static MatrixStack *GLProjection() {
-        if (__gl_projection == NULL) {
-            __gl_projection = new MatrixStack();
-        }
-        return __gl_projection;
-    }
-
-    static MatrixStack *GLModelView() {
-        if (__gl_model_view == NULL) {
-            __gl_model_view = new MatrixStack();
-        }
-        return __gl_model_view;
-    }
-
-    static MatrixStack *GLTexture() {
-        if (__gl_texture == NULL) {
-            __gl_texture = new MatrixStack();
-        }
-        return __gl_texture;
-    }
-
+    static MatrixStack *GLProjection();
+    static MatrixStack *GLModelView();
+    static MatrixStack *GLTexture();
     static void Purge();
 
-    mat4& Push() {
-        stack_.push_back(stack_.empty() ? mat4::Identity() : Top());
-        return stack_.back();
-    }
-
-    mat4 Pop() {
-        mat4 result = stack_.back();
-        stack_.pop_back();
-        return result;
-    }
-
-    mat4& Top() {
-        return stack_.back();
-    }
+    mat4& Push();
+    mat4 Pop();
+    mat4& Top();
 
 private:
     MatrixStack() : stack_() {
