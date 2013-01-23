7137f7e829a2646a036d77665ae698b7ebcea057
f-kubotar <dev@hadashikick.jp>
2013-01-23 18:41:37 +0900

    rename EventManager -> EventDispatcher

---
+++ event_dispatcher.cc
@@ -0,0 +1,1 @@
+#include "event_dispatcher.h"
---
+++ event_dispatcher.h
@@ -0,0 +1,11 @@
+#ifndef __kawaii__event_dispatcher__
+#define __kawaii__event_dispatcher__
+
+namespace kawaii {
+
+class EventDispatcher {
+};
+
+}
+
+#endif /* defined(__kawaii__event_dispacher__) */
