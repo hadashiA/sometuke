bf07ab6fa9febafff138ef4b27f527c4fc25d7b0
f-kubotar <dev@hadashikick.jp>
2013-01-23 22:41:40 +0900

    Revert "Revert "MatrixStackもinline化""
    This reverts commit b326af4559330e05a57dd47a87458264f033f561.

--- matrix_stack.cc
+++ matrix_stack.cc
@@ -6,26 +6,6 @@ MatrixStack *MatrixStack::__gl_projection = NULL;
 MatrixStack *MatrixStack::__gl_model_view = NULL;
 MatrixStack *MatrixStack::__gl_texture    = NULL;
 
-MatrixStack *MatrixStack::GLProjection() {
-    if (__gl_projection == NULL) {
-        __gl_projection = new MatrixStack();
-    }
-    return __gl_projection;
-}
-
-MatrixStack *MatrixStack::GLModelView() {
-    if (__gl_model_view == NULL) {
-        __gl_model_view = new MatrixStack();
-    }
-    return __gl_model_view;
-}
-
-MatrixStack *MatrixStack::GLTexture() {
-    if (__gl_texture == NULL) {
-        __gl_texture = new MatrixStack();
-    }
-    return __gl_texture;
-}
 
 void MatrixStack::Purge() {
     if (__gl_projection != NULL) {
@@ -42,26 +22,6 @@ void MatrixStack::Purge() {
         delete __gl_texture;
         __gl_texture = NULL;
     }
-}
-
-mat4& MatrixStack::Push() {
-    if (stack_.empty()) {
-        stack_.push_back(mat4::Identity());
-    } else {
-        stack_.push_back(Top());
-    }
-    return stack_.back();
-}
-
-mat4 MatrixStack::Pop() {
-    mat4 result = stack_.back();
-    stack_.pop_back();
-
-    return result;
-}
-
-mat4& MatrixStack::Top() {
-    return stack_.back();
 }
 
 }
