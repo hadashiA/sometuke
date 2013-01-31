31a63e95ae874fb62d323e87d4d9e989b449a340
f-kubotar <dev@hadashikick.jp>
2013-02-01 02:31:31 +0900

    added sprite_frame_cache

---
+++ sprite_frame_cache.cc
@@ -0,0 +1,11 @@
+#include "sprite_frame_cache.h"
+
+#include "sprite_frame.h"
+
+namespace kawaii {
+
+SpriteFrameCache *SpriteFrameCache::__shared = NULL;
+
+
+
+}
---
+++ sprite_frame_cache.h
@@ -0,0 +1,33 @@
+#ifndef __kawaii__sprite_frame_cache__
+#define __kawaii__sprite_frame_cache__
+
+#include "hashed_string.h"
+
+#include <map>
+
+namespace kawaii {
+
+class SpriteFrame;
+
+class SpriteFrameCache {
+public:
+    static inline SpriteFrameCache *Shared() {
+        if (__shared == NULL) {
+            __shared = new SpriteFrameCache;
+        }
+        return __shared;
+    }
+
+private:
+    typedef std::map<unsigned int, shared_ptr<SpriteFrame> > SpriteFrameTable;
+
+    static SpriteFrameCache *__shared;
+
+    SpriteFrameCache() {}
+
+    SpriteFrameTable sprite_frames_;
+};
+
+}
+
+#endif /* defined(__kawaii__sprite_frame_cache__) */
