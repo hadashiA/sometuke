a1ad5a4bf3fae00c6081125e887e6696f005e56f
f-kubotar <dev@hadashikick.jp>
2013-02-11 18:03:28 +0900

    Animation::operator[]

--- animation.h
+++ animation.h
@@ -28,6 +28,10 @@ struct Animation {
           frames(f) {
     }
 
+    const AnimationFrame operator[](int index) const {
+        return frames[index];
+    }
+
     unsigned int frame_size;
     bool loops;
     vector<AnimationFrame> frames;
