37c166451ec55147efb5c335f8da50d46a4fd128
f-kubotar <dev@hadashikick.jp>
2013-03-11 22:30:09 +0900

    implemented animate loops count

--- animate.cc
+++ animate.cc
@@ -1,6 +1,8 @@
 #include "animate.h"
 #include "sprite.h"
 
+#include "logger.h"
+
 namespace kawaii {
 
 const HashedString Animate::TYPE("animate");
@@ -12,8 +14,15 @@ void Animate::OnEnter() {
 void Animate::Update(ii_time delta_time) {
     ii_time current_frame_time = animation_->frames[frame_num_].time;
     if (elapsed_ > current_frame_time) {
-        frame_num_ = (frame_num_ + 1) % animation_->frame_size;
         elapsed_ = 0;
+        
+        if (++frame_num_ >= animation_->frame_size) {
+            if (++executed_loops_ >= animation_->loops) {
+                Kill();
+                return;
+            }
+            frame_num_ = 0;
+        }
 
         shared_ptr<SpriteFrame> sprite_frame = animation_->frames[frame_num_].sprite_frame;
         target_->set_display_frame(sprite_frame);
