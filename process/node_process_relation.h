fca9768d184b0168c7db8f083a919ea14aa43cb2
f-kubotar <dev@hadashikick.jp>
2013-03-29 01:55:22 +0900

    add NodeProcessRelation

---
+++ process/node_process_relation.h
@@ -0,0 +1,39 @@
+#ifndef __kawaii__process_target__
+#define __kawaii__process_target__
+
+#include "application.h"
+#include "memory_pool.h"
+
+#include <memory>
+
+namespace kawaii {
+
+class Node;
+class Sequence;
+
+class NodeProcessRelation : public GeneralPoolable {
+public:
+    NodeProcessRelation(weak_ptr<Node> target)
+        : target_(target) {}
+
+    virtual ~NodeProcessRelation() {
+        Application::Instance().director().scheduler().ScheduleFor(sequence_);
+    }
+
+private:
+    weak_ptr<Node> target_;
+    shared_ptr<Process> sequence_;
+};
+
+NodeProcessRelation Target(weak_ptr<Node> target) {
+    return NodeProcessRelation(target);
+}
+
+}
+
+#endif /* defined(__kawaii__process_target__) */
+
+
+
+
+
