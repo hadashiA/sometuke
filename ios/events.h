23804ff0c78d13d43dbc44cbb5b5fac7efc9e622
f-kubotar <dev@hadashikick.jp>
2013-04-07 20:19:31 +0900

    TouchPhase

--- ios/events.h
+++ ios/events.h
@@ -6,16 +6,25 @@
 
 namespace kawaii {
 
+typedef enum {
+    kTouchBegan,
+    kTouchMoved,
+    kTouchEnded,
+    kTouchCancelled,
+} TouchPhase;
+
 struct TouchEvent : public Event {
     static const EventType TYPE;
 
-    TouchEvent(const vec2& loc, const vec2& prev, size_t cnt)
-    : Event(TYPE),
+    TouchEvent(TouchPhase _phase, const vec2& loc, const vec2& prev, size_t cnt)
+        : Event(TYPE),
+          phase(_phase),
           location(loc),
           prev_location(prev),
           tap_count(cnt) {
     }
 
+    TouchPhase phase;
     vec2 location;
     vec2 prev_location;
     size_t tap_count;
