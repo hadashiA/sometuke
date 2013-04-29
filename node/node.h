aebb911cf2d7af87878441776f8449d048547b34
f-kubotar <dev@hadashikick.jp>
2013-04-29 14:16:48 +0900

    add helper method

--- node/node.h
+++ node/node.h
@@ -176,6 +176,10 @@ public:
         }
     }
 
+    void set_anchor_point(const float x, const float y) {
+        set_anchor_point(vec2(x, y));
+    }
+
     void set_content_size(const vec2& value) {
         if (content_size_ != value) {
             content_size_ = value;
@@ -183,6 +187,10 @@ public:
                                            content_size_.y * anchor_point_.y);
             is_transform_dirty_ = is_inverse_dirty_ = true;
         }
+    }
+
+    void set_content_size(const float x, const float y) {
+        set_content_size(vec2(x, y));
     }
 
     void show() {
