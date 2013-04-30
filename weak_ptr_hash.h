1c82fd05ad63938951b81b7cdb953debf4613b74
f-kubotar <dev@hadashikick.jp>
2013-05-01 01:18:28 +0900

    add weak_ptr_hash helpers

---
+++ weak_ptr_hash.h
@@ -0,0 +1,31 @@
+#ifndef __kawaii__weak_ptr_hash__
+#define __kawaii__weak_ptr_hash__
+
+#include <memory>
+#include <functional>
+
+namespace kawaii {
+using namespace std;
+
+template<typename T>
+struct WeakPtrHash : public unary_function<weak_ptr<T>, size_t> {
+   size_t operator()(const weak_ptr<T>& wp) {
+       // Example hash, can't guarantee that this is any good though...
+       if (shared_ptr<T> sp = wp.lock()) {
+           return std::hash<shared_ptr<T> >()(sp);
+       } else {
+           return 0;
+       }
+   }
+};
+
+template<typename T>
+struct WeakPtrEqual : public unary_function<weak_ptr<T>, bool> {
+    bool operator()(const std::weak_ptr<T>& left, const std::weak_ptr<T>& right) {
+        return !left.owner_before(right) && !right.owner_before(left);
+    }
+};
+
+}
+
+#endif /* defined(__kawaii__weak_ptr_hash__) */
