c350e9817ebff3d56208fedf8bbc4bb279609a33
f-kubotar <dev@hadashikick.jp>
2013-05-03 19:12:32 +0900

    add Animation::Push()

--- animation.h
+++ animation.h
@@ -3,17 +3,18 @@
 
 #include "kawaii/types.h"
 #include "kawaii/event_dispatcher.h"
+#include "kawaii/sprite_frame_cache.h"
+#include "kawaii/logger.h"
 
 #include <vector>
 #include <memory>
+#include <string>
 
 namespace kawaii {
 using namespace std;
 
-class SpriteFrame;
-
 struct AnimationFrame {
-    AnimationFrame(shared_ptr<SpriteFrame> f, ii_time t, const char *e = "")
+    AnimationFrame(shared_ptr<SpriteFrame> f, ii_time t, shared_ptr<Event> e = NULL)
         : sprite_frame(f),
           time(t) {
     }
@@ -27,6 +28,23 @@ struct Animation {
         : frame_size(f.size()),
           loops(l),
           frames(f) {
+    }
+
+    explicit Animation(unsigned int l=1)
+        : frame_size(),
+          loops(l) {
+    }
+
+    void Push(const char *sprite_frame_name, ii_time t, shared_ptr<Event> e = NULL) {
+        SpriteFrameCache& cache = SpriteFrameCache::Instance();
+        shared_ptr<SpriteFrame> sprite_frame = cache[sprite_frame_name];
+        if (!sprite_frame) {
+            IIERROR("sprite frame name %s not found", sprite_frame_name);
+        }
+
+        AnimationFrame frame(sprite_frame, t, e);
+        frames.push_back(frame);
+        frame_size = frames.size();
     }
 
     unsigned int frame_size;
