564a3479fefe073abf9d2cecccd69e25e1984d7c
f-kubotar <dev@hadashikick.jp>
2013-07-19 03:35:57 +0900

    デフォルトのイベント消した。アプリ側で全部定義する

--- events.cc
+++ events.cc
@@ -2,8 +2,6 @@
 
 namespace skidarake {
 
-const EventType MoveActorEvent::TYPE("move:actor");
-const EventType RotateActorEvent::TYPE("rotate:actor");
 const EventType AccelerationEvent::TYPE("acceleration");
 
 }
--- events.h
+++ events.h
@@ -29,43 +29,6 @@ struct Event {
     time_t timestamp;
 };
 
-struct MoveActorEvent : public Event {
-    static const EventType TYPE;
-
-    MoveActorEvent(const ActorId& id, const vec2& pos)
-        : Event(TYPE),
-          actor_id(id),
-          position(pos) {
-    }
-
-    ActorId actor_id;
-    vec2 position;
-};
-
-struct RotateActorEvent : public Event {
-    static const EventType TYPE;
-
-    RotateActorEvent(const ActorId& id, const float degrees)
-        : Event(TYPE),
-          actor_id(id),
-          rotate(degrees) {
-    }
-
-    ActorId actor_id;
-    float rotate;
-};
-
-struct AccelerationEvent : public Event {
-    static const EventType TYPE;
-
-    AccelerationEvent(const vec3& v)
-        : Event(TYPE),
-          gravity(v) {
-    }
-
-    vec3 gravity;
-};
-
 }
 
 #endif /* defined(__skidarake__events__) */
