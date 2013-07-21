40a9ee3a6dc7fd20073bc10207be7082f977fee1
f-kubotar <dev@hadashikick.jp>
2013-07-21 23:21:20 +0900

    Updateを使うオブジェクトは原則TimerDelegateつかうことにした。UpdateInterface消した

--- scheduler.cc
+++ scheduler.cc
@@ -1,23 +1,20 @@
 #include "sometuke/scheduler.h"
 
-#include "sometuke/process/process_manager.h"
-
 #include <climits>
 
 namespace sometuke {
-
 const unsigned int Timer::REPEAT_FOREVER(UINT_MAX - 1);
 
-bool Timer::Update(const ii_time delta_time) {
+bool Timer::Tick(const ii_time delta_time) {
     // standard timer usage
     if (run_forever_ && !use_delay_) {
         elapsed_ += delta_time;
         if (elapsed_ >= interval_) {
             elapsed_ = 0;
-            return delegate_->Update(elapsed_);
+            return Update(elapsed_);
         }
         
-    // advanced usage
+        // advanced usage
     } else {
         elapsed_ += delta_time;
         if (use_delay_) {
@@ -25,13 +22,13 @@ bool Timer::Update(const ii_time delta_time) {
                 elapsed_ -= delay_;
                 num_executed_++;
                 use_delay_ = false;
-                return delegate_->Update(elapsed_);
+                return Update(elapsed_);
             }
         } else {
             if (elapsed_ >= interval_) {
                 elapsed_ = 0;
                 num_executed_++;
-                return delegate_->Update(elapsed_);
+                return Update(elapsed_);
             }
         }
         
@@ -41,40 +38,4 @@ bool Timer::Update(const ii_time delta_time) {
     }
     return true;
 }
-
-
-void Scheduler::Schedule(shared_ptr<UpdateInterface> delegate) {
-    update_list_.push_back(delegate);
-}
-
-void Scheduler::Schedule(shared_ptr<UpdateInterface> delegate, const ii_time interval) {
-    shared_ptr<UpdateInterface> timer = make_shared<Timer>(delegate, interval);
-    Schedule(timer);
-}
-    
-void Scheduler::Schedule(shared_ptr<UpdateInterface> delegate,
-                         const ii_time interval,
-                         const unsigned int repeat,
-                         const ii_time delay) {
-    shared_ptr<UpdateInterface> timer = make_shared<Timer>(delegate, interval, repeat, delay);
-    Schedule(timer);
-}
-
-void Scheduler::Unschedule(shared_ptr<UpdateInterface> delegate) {
-    update_list_.remove(delegate);
-}
-
-void Scheduler::Update(const ii_time delta_time) {
-    for (UpdateList::iterator i = update_list_.begin(); i != update_list_.end();) {
-        shared_ptr<UpdateInterface> p = (*i);
-        
-        if (!p->Update(delta_time)) {
-            update_list_.erase(i);
-        } else {
-            ++i;
-
-        }
-    }
-}
-
 }
--- scheduler.h
+++ scheduler.h
@@ -11,60 +11,52 @@
 namespace sometuke {
 using namespace std;
 
-class UpdateInterface {
+class Timer : enable_shared_from_this<Timer> {
 public:
-    virtual ~UpdateInterface() {}
+    Timer(const ii_time interval = 0,
+          const unsigned int repeat = REPEAT_FOREVER,
+          const ii_time delay = 0)
+        : paused_(false) {
+        set_interval(interval);
+        set_repeat(repeat);
+        set_delay(delay);
+    }
+
+    virtual ~Timer() {}
+
+    virtual bool Tick(const ii_time delta_time);
     virtual bool Update(const ii_time delta) = 0;
-};
 
-class ProcessManager;
-
-class Timer : public UpdateInterface {
-public:
-    static const unsigned int REPEAT_FOREVER;
-    
-    Timer(shared_ptr<UpdateInterface> delegate)
-        : delegate_(delegate),
-          elapsed_(0),
-          num_executed_(0),
-          interval_(0),
-          delay_(0),
-          use_delay_(false),
-          repeat_(REPEAT_FOREVER),
-          run_forever_(true) {
+    void set_interval(const ii_time interval) {
+        interval_ = interval;
+        elapsed_  = 0;
     }
 
-    Timer(shared_ptr<UpdateInterface> delegate, const ii_time interval)
-        : delegate_(delegate),
-          elapsed_(0),
-          num_executed_(0),
-          interval_(interval),
-          delay_(0),
-          use_delay_(false),
-          repeat_(REPEAT_FOREVER),
-          run_forever_(true) {
+    void set_repeat(const unsigned int repeat) {
+        repeat_ = repeat;
+        run_forever_ = (repeat == REPEAT_FOREVER);
+        num_executed_ = 0;
     }
 
-    Timer(shared_ptr<UpdateInterface> delegate, const ii_time interval,
-          const unsigned int repeat, const ii_time delay)
-        : delegate_(delegate),
-          elapsed_(0),
-          num_executed_(0),
-          interval_(interval),
-          delay_(delay),
-          use_delay_(delay > 0),
-          repeat_(repeat),
-          run_forever_(repeat == REPEAT_FOREVER) {
+    void set_delay(const ii_time delay) {
+        delay_ = delay;
+        use_delay_ = (delay > 0);
     }
 
-    virtual bool Update(const ii_time delta);
+    const bool paused() const { return paused_; }
+    void Pause()  { paused_ = true; }
+    void Resume() { paused_ = false; }
 
-    shared_ptr<UpdateInterface> delegate() {
-        return delegate_;
+    void Schedule() {
+        Application::Instance().scheduler().Schedule(shared_from_this());
     }
 
-private:    
-    shared_ptr<UpdateInterface> delegate_;
+    void UnSchedule() {
+        Application::Instance().scheduler().UnSchedule(shared_from_this());
+    }
+
+private:
+    bool paused_;
     ii_time elapsed_;
     ii_time interval_;
     ii_time delay_;
@@ -74,22 +66,67 @@ private:
     int num_executed_;
 };
 
-typedef list<shared_ptr<UpdateInterface> > UpdateList;
+class ProcessManager;
+
+template <class T>
+class TimerDelegator : public UpdateInterface {
+public:
+    static const unsigned int REPEAT_FOREVER;
+    
+    static shared_ptr<Timer> Create(T *handler,
+                                    const ii_time interval = 0,
+                                    const unsigned int repeat = REPEAT_FOREVER,
+                                    const ii_time delay = 0) {
+        shared_ptr<T> handler_ptr = static_pointer_cast<T>(handler->shared_from_this());
+        return make_shared<TimerDelegator<T> >(handler_ptr, interval, repeat, delay);
+    }
+
+    TimerDelegate(weak_ptr<T> delegate,
+                  const ii_time interval = 0,
+                  const unsigned int repeat = REPEAT_FOREVER,
+                  const ii_time delay = 0)
+        : Timer(interval, repeat, delay),
+          delegate_(delegate) {
+    }
+
+    virtual bool Update(const ii_time delta) {
+        if (shared_ptr<T> delegate = delegate_.lock()) {
+            return delegate->Update(delta);
+        } else {
+            return false;
+        }
+    }
+
+private:    
+    weak_ptr<T> delegate_;
+};
+
+typedef list<shared_ptr<Timer> > TimerList;
 
 class Scheduler {
 public:
-    void Schedule(shared_ptr<UpdateInterface> delegate);
-    void Schedule(shared_ptr<UpdateInterface> delegate, const ii_time interval);
-    void Schedule(shared_ptr<UpdateInterface> delegate, const ii_time interval,
-                  const unsigned int repeat, const ii_time delay);
+    void Schedule(shared_ptr<Timer> timer) {
+        timers_.push_back(timer);
+    }
 
-    void Unschedule(shared_ptr<UpdateInterface> delegate);
-
-    void Update(const ii_time delta_time);
+    void Unschedule(const shared_ptr<UpdateInterface>& timer) {
+        timers_list_.remove(timer);
+    }
+    
+    void Update(const ii_time delta_time) {
+        for (TimerList::iterator i = timers_.begin(); i != timers_.end();) {
+            shared_ptr<Timer> timer = (*i);
+            
+            if (!timer->Tick(delta_time)) {
+                timer_list_.erase(i);
+            } else {
+                ++i;
+            }
+        }
+    }
 
 private:
-    UpdateList update_list_;
-    shared_ptr<ProcessManager> process_manager_;
+    TimerList timers_;
 };
 
 }
