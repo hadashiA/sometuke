e66a9c41fc20fbe8cf956e78fa548a120c0cf632
f-kubotar <dev@hadashikick.jp>
2013-07-26 02:43:37 +0900

    いらないファイル消して整理した

--- ios/ios.h
+++ ios/ios.h
@@ -1,3 +1,25 @@
-#include "sometuke/ios/ios_component.h"
-#include "sometuke/touch_dispatcher.h"
+#ifndef __sometuke__ios_component_factory__
+#define __sometuke__ios_component_factory__
 
+#include "sometuke/application_component.h"
+#include "sometuke/ios/ios_assets_loader.h"
+#include "sometuke/ios/ios_system_font_loader.h"
+
+namespace sometuke {
+
+class IOSComponentFactory : public ApplicationComponentFactory {
+public:
+    virtual ~IOSComponentFactory() {}
+
+    virtual AssetsLoader *CreateAssetsLoader() {
+        return new IOSAssetsLoader;
+    }
+
+    virtual SystemFontLoader *CreateSystemFontLoader() {
+        return new IOSSystemFontLoader;
+    }
+};
+
+}
+
+#endif /* defined(__sometuke__ios_component_factory__) */
