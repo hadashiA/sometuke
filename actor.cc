799abc77e5e978ffe084c0d8d23054888dedeb93
f-kubotar <dev@hadashikick.jp>
2013-08-22 23:53:20 +0900

    デフォルトでactorにプロパティなにも持たせないようにした

--- actor.cc
+++ actor.cc
@@ -2,8 +2,6 @@
 
 namespace sometuke {
 
-const EventType ActorMoveEvent::TYPE("actor:move");
-const EventType ActorRotateEvent::TYPE("actor:rotate");
 const EventType ActorAddEvent::TYPE("actor:add");
 const EventType ActorRemoveEvent::TYPE("actor:remove");
 
--- actor.h
+++ actor.h
@@ -37,39 +37,11 @@ struct ActorRemoveEvent : public Event {
     ActorId actor_id;
 };
 
-struct ActorMoveEvent : public Event {
-    static const EventType TYPE;
-
-    ActorMoveEvent(const ActorId& id, const vec3& pos)
-        : Event(TYPE),
-          actor_id(id),
-          location(pos) {
-    }
-
-    ActorId actor_id;
-    vec3 location;
-};
-
-struct ActorRotateEvent : public Event {
-    static const EventType TYPE;
-
-    ActorRotateEvent(const ActorId& id, const float degrees)
-        : Event(TYPE),
-          actor_id(id),
-          rotate(degrees) {
-    }
-
-    ActorId actor_id;
-    float rotate;
-};
-
 class Actor : public Handler {
 public:
     Actor(const ActorType& t)
         : type_(t),
-          id_(),
-          location_(0, 0, 0),
-          rotation_(0) {
+          id_()
         id_.Generate();
     }
 
@@ -85,38 +57,9 @@ public:
         return type_;
     }
 
-    const vec3& location() const {
-        return location_;
-    }
-
-    const float rotation() const {
-        return rotation_;
-    }
-
-    void set_location(const float x, const float y) {
-        set_location(vec3(x, y, 1));
-    }
-
-    void set_location(const vec3& location) {
-        if (location_ != location) {
-            location_ = location;
-            Director::Instance().dispatcher().Queue<ActorMoveEvent>(id_, location);
-        }
-    }
-
-    void set_rotation(const float rotation) {
-        if (rotation_ != rotation) {
-            rotation_ = rotation;
-            Director::Instance().dispatcher().Queue<ActorRotateEvent>(id_, rotation);
-        }
-    }
-
 protected:
     ActorId id_;
     HashedString type_;
-
-    vec3 location_;
-    float rotation_;
 };
 
 }
